13cb12eed5a58322677407431bfbc58a9db569d7
duclong1311 <duclong1311.work@gmail.com>
2025-07-16 00:01:32 +0700

    config: axios

---
+++ src/libs/axios.ts
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:3000/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      console.error('Unauthorized access');
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
