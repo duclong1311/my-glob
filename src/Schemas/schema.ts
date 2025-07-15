33f60639cd47a2fa82b3de7b9ab9ce94473f6f7d
duclong1311 <duclong1311.work@gmail.com>
2025-07-16 00:44:55 +0700

    format: master format all file

--- src/Schemas/schema.ts
+++ src/Schemas/schema.ts
@@ -2,8 +2,8 @@
 import { z } from 'zod';
 
 export const userSchema = z.object({
-  name: z.string().min(1, 'Name is required'),  // Dữ liệu tên phải là chuỗi và không trống
-  age: z.number().min(18, 'Age must be at least 18'),  // Tuổi phải là số và >= 18
+  name: z.string().min(1, 'Name is required'), // Dữ liệu tên phải là chuỗi và không trống
+  age: z.number().min(18, 'Age must be at least 18'), // Tuổi phải là số và >= 18
 });
 
 export type User = z.infer<typeof userSchema>;
--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -8,26 +8,26 @@ const axiosInstance = axios.create({
 });
 
 axiosInstance.interceptors.request.use(
-  (config) => {
+  config => {
     const token = localStorage.getItem('authToken');
     if (token) {
       config.headers['Authorization'] = `Bearer ${token}`;
     }
     return config;
   },
-  (error) => {
+  error => {
     return Promise.reject(error);
-  }
+  },
 );
 
 axiosInstance.interceptors.response.use(
-  (response) => response,
-  (error) => {
+  response => response,
+  error => {
     if (error.response && error.response.status === 401) {
       console.error('Unauthorized access');
     }
     return Promise.reject(error);
-  }
+  },
 );
 
 export default axiosInstance;
